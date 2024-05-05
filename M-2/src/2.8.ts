0ab185adf19e43e3a9d55021dd22c31d955fe937
rabby-web <rabby.webdeveloper@gmail.com>
2024-05-05 07:53:36 +0600

    promise to json

---
+++ M-2/src/2.8.ts
@@ -0,0 +1,43 @@
+{
+  // promise
+  type ToDo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+
+  const getTodo = async (): Promise<ToDo> => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    return data;
+    // console.log(data);
+  };
+  getTodo();
+  //   const info = getTodo();
+  //   console.log(info);
+
+  type Something = { something: string };
+  // simulate
+  const createPromise = (): Promise<Something> => {
+    return new Promise<Something>((resolve, reject) => {
+      const data: Something = { something: "something" };
+      if (data) {
+        resolve(data);
+      } else {
+        reject("failed to load data");
+      }
+    });
+  };
+  //   calling create promise function
+  const showData = async (): Promise<Something> => {
+    const data: Something = await createPromise();
+    console.log(data);
+    return data;
+  };
+  showData();
+
+  //
+}
