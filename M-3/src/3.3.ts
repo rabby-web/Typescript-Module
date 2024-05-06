67339655e5de9805d3259126275cbdadf233dfc1
rabby-web <rabby.webdeveloper@gmail.com>
2024-05-06 15:56:40 +0600

    typeof ==> type guard

---
+++ M-3/src/3.3.ts
@@ -0,0 +1,18 @@
+{
+  //  type guards
+  // typeof ==> type guard
+
+  type Alpha = string | number;
+
+  const add = (param1: Alpha, param2: Alpha): Alpha => {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
+  };
+  const result1 = add("2", " 3");
+//   console.log(result1);
+
+  //
+}
