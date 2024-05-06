30abebd1c6ebb26077dc6cb2e6e2996459731d09
rabby-web <rabby.webdeveloper@gmail.com>
2024-05-06 20:33:53 +0600

    getter and setter in tsc

--- M-3/src/3.6.ts
+++ M-3/src/3.6.ts
@@ -1,3 +1,37 @@
 {
-    
+  // getter and setter
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    // private _balance: number;
+    protected _balance: number;
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+    // ========> function
+    // public addDeposit(amount: number) {
+    //   this._balance = this._balance + amount;
+    // }
+    // public getBalance() {
+    //   return this._balance;
+    // }
+    // getter
+    get balance() {
+      return this._balance;
+    }
+    set deposit(amount: number) {
+      this._balance = this.balance + amount;
+    }
+  }
+
+  const poorMA = new BankAccount(11, "Mr. Poor", 50);
+  //   poorMA.addDeposit(300);
+  //   const myBalance = poorMA.getBalance();
+  poorMA.deposit = 40;
+  const myBalance = poorMA.balance;
+  console.log(myBalance);
+
+  //
 }
